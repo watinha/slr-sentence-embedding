95fac0661aaddc6867f10fab58b5d6bab87e7b93
Willian Massami Watanabe <watinha@gmail.com>
2022-12-15 21:50:17 -0300

    varying threshold and calculating SLR updates metrics

--- main.py
+++ main.py
@@ -36,8 +36,13 @@ slr_files = get_slr_files(theme)
 X, y, years = load(slr_files, titles_only=titles)
 
 kfold = YearsSplit(n_split=3, years=years)
+
 y_pred = []
 y_true = []
+y_pred_prob = []
+excluded = []
+missed = []
+
 X = np.array(X)
 y = np.array(y)
 
@@ -62,6 +67,31 @@ for train_index, test_index in kfold.split(X, y):
     y_pred = y_pred + (pipeline.predict(X_test)).tolist()
     y_true = y_true + y_test.tolist()
 
+    true_class_index = list(pipeline.classes_).index(1)
+    y_prob = pipeline.predict_proba(X_train)[:, true_class_index]
+    _, _, thresholds = metrics.precision_recall_curve(y_train, y_prob)
+    threshold = min(thresholds[0], 0.5)
+    y_test_prob = (pipeline.predict_proba(X_test)[:, true_class_index])
+    y_test_prob = [ 0 if prob < threshold else 1 for prob in y_test_prob.tolist() ]
+    y_pred_prob = y_pred_prob + y_test_prob
+
+    matrix = metrics.confusion_matrix(y_test, y_test_prob)
+    excluded.append(
+      matrix[0, 0] /
+      (matrix[0, 0] + matrix[1, 1] + matrix[0, 1] + matrix[1, 0]))
+    missed.append(matrix[1, 0] / (matrix[1, 1] + matrix[1, 0]))
+
+
 print(metrics.classification_report(y_true, y_pred))
+print('')
+print('Configuring the activation threshold')
+print('')
+print(metrics.classification_report(y_true, y_pred_prob))
+print('')
+print('SLR Updates metrics')
+print('')
+print('Excluded percentage: %f' % (sum(excluded) / len(excluded)))
+print('  Missed percentage: %f' % (sum(missed) / len(missed)))
+
 
 sys.exit(0)
