4a92c6da6fc5df53e604d976489a8d3f134a1b10
Willian Massami Watanabe <watinha@gmail.com>
2022-12-16 11:00:18 -0300

    word index should contain all words

--- test/test_SE_loader.py
+++ test/test_SE_loader.py
@@ -6,36 +6,33 @@ from util.embedding_vectorizer import SELoader
 class SELoaderTest (TestCase):
 
     def test_loads_embeddings (self):
-        word_list = set([])
         loader = SELoader('../SE_embeddings.bin')
         wv_mock = Mock()
 
         with patch('gensim.models.KeyedVectors.load_word2vec_format') as mock:
             mock.return_value = wv_mock
-            result = loader.build_word_index(word_list)
-            self.assertEqual({}, result)
+            result = loader.build_word_index()
+            self.assertEqual(wv_mock, result)
             mock.assert_called_once_with(
                     '../SE_embeddings.bin', binary=True)
 
+
     def test_loads_embeddings_and_retrieves_vector (self):
-        word_list = set(['abobrinha'])
         loader = SELoader('../SE_embeddings2.bin')
         wv_mock = Mock()
         wv_mock.get_vector.return_value = [1, 2, 3]
 
         with patch('gensim.models.KeyedVectors.load_word2vec_format') as mock:
             mock.return_value = wv_mock
-            result = loader.build_word_index(word_list)
+            result = loader.build_word_index()
+
             mock.assert_called_once_with(
                     '../SE_embeddings2.bin', binary=True)
+            self.assertEqual([1, 2, 3], result.get_vector('abobrinha'))
             wv_mock.get_vector.assert_called_once_with('abobrinha')
-            self.assertIn('abobrinha', result)
-            self.assertEqual(
-                    [1, 2, 3], result['abobrinha'].tolist())
 
 
     def test_retrieves_vector_but_ignores_inexistent_words (self):
-        word_list = set(['pepino', 'limao', 'beterraba'])
         loader = SELoader('../SE_embeddings2.bin')
         wv_mock = Mock()
         fake_word_index = {
@@ -46,16 +43,11 @@ class SELoaderTest (TestCase):
 
         with patch('gensim.models.KeyedVectors.load_word2vec_format') as mock:
             mock.return_value = wv_mock
-            result = loader.build_word_index(word_list)
+            result = loader.build_word_index()
             mock.assert_called_once_with(
                     '../SE_embeddings2.bin', binary=True)
+            self.assertEqual([3, 2, 1], result.get_vector('pepino'))
+            self.assertEqual([6, 5, 4], result.get_vector('beterraba'))
             wv_mock.get_vector.assert_any_call('pepino')
-            wv_mock.get_vector.assert_any_call('limao')
             wv_mock.get_vector.assert_any_call('beterraba')
-            self.assertIn('pepino', result)
-            self.assertEqual(
-                    [3, 2, 1], result['pepino'].tolist())
-            self.assertIn('beterraba', result)
-            self.assertEqual(
-                    [6, 5, 4], result['beterraba'].tolist())
 
--- util/embedding_vectorizer.py
+++ util/embedding_vectorizer.py
@@ -16,8 +16,7 @@ class AverageEmbeddingVectorizer():
 
     def transform(self, X):
         sentences = [word_tokenize(row) for row in X]
-        word_list = set(sum(sentences, []))
-        word_index = self._loader.build_word_index(word_list)
+        word_index = self._loader.build_word_index()
 
         result = []
         for sentence in sentences:
@@ -53,14 +52,13 @@ class GloveLoader:
     def __init__ (self, filename):
         self._filename = filename
 
-    def build_word_index (self, word_list):
+    def build_word_index (self):
         word_index = {}
         with open(self._filename) as f:
             for line in f:
                 word, *vector = line.split()
-                if word in word_list:
-                    word_index[word] = np.array(
-                            vector, dtype=np.float32)
+                word_index[word] = np.array(
+                        vector, dtype=np.float32)
         return word_index
 
 
@@ -69,16 +67,9 @@ class SELoader:
     def __init__ (self, filename):
         self._filename = filename
 
-    def build_word_index (self, word_list):
+    def build_word_index (self):
         word_index = {}
         word_vecs = KeyedVectors.load_word2vec_format(
                 self._filename, binary=True)
 
-        for word in word_list:
-            try:
-                word_index[word] = np.array(
-                        word_vecs.get_vector(word), dtype=np.float32)
-            except KeyError:
-                pass
-
-        return word_index
+        return word_vecs
